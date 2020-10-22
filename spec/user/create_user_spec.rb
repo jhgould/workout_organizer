4ad3c4bcbee15bf310744ea6349e0f5ba353e6be
Jack Gould <johnhilsmangould@gmail.com>
2020-10-22 11:32:04 -0600

    add test for adding user

---
+++ spec/user/create_user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+RSpec.describe 'create, update, login, logout ', type: :feature do
+  it "vistor can create a user" do 
+    visit '/'
+
+    click_on "Create User"
+
+    expect(current_path).to eq('/users/new')
+
+    username = "test_user"
+    password = "password"
+
+    fill_in :username, with: username
+    fill_in :password, with: password
+
+    click_on "Create"
+
+    expect(current_path).to eq('/')
+    expect(page).to have_content("Welcome, #{username}")
+  end 
+
+   
+end
