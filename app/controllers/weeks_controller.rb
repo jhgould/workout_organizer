d16c2f8ca52df011aeb972e3d86c13cc2b1a66fe
Jack Gould <johnhilsmangould@gmail.com>
2020-11-03 11:42:31 -0700

    add week controller methods and views

---
+++ app/controllers/weeks_controller.rb
@@ -0,0 +1,12 @@
+class WeeksController < ApplicationController
+  def new 
+    @id = params[:id]
+  end 
+
+  def create 
+    workout = Workout.find(params[:id])
+    workout.weeks.number_of_weeks(params[:num_weeks], workout)
+    redirect_to "/workouts/#{workout.id}"
+  end 
+
+end 
