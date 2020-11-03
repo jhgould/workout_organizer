8150dddcac624831003db4be042da6ab4336d449
Jack Gould <johnhilsmangould@gmail.com>
2020-11-03 15:06:11 -0700

    add controller methods and model methods for day creation

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,12 @@
 class Day < ApplicationRecord 
   belongs_to :week
 
+  def self.create_days(week)
+    counter = 1
+    7.times do 
+      Day.create(name: "Day #{counter}", week: week)
+      counter += 1
+    end 
+  end 
+
 end 
