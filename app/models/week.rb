e6584755e7f73601b9275d158f37af640d653027
Jack Gould <johnhilsmangould@gmail.com>
2020-11-03 11:41:59 -0700

    add create week routes and model method to create many at once

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,4 +1,13 @@
 class Week < ApplicationRecord
     belongs_to :workout
 
+    def self.number_of_weeks(num, workout)
+        counter = 1
+        num.to_i.times do 
+            Week.create(name: "Week #{counter}", workout: workout )
+            counter += 1
+        end 
+    end
+
+
 end 
