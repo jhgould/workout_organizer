b6ae16f0ba0f68a2d5e3914f0bae9034407b15b7
Jack Gould <johnhilsmangould@gmail.com>
2020-11-10 08:29:58 -0700

    comment out an un-needed method

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,7 @@
 class WorkoutsController < ApplicationController 
   def index 
     @user = User.find(session[:user_id])
-    @workouts = Workout.all
+    # @workouts = Workout.all
   end 
 
   def show 
