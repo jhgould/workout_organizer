567c6353c8a6a0e06f3975690d3c683405cdef61
Jack Gould <johnhilsmangould@gmail.com>
2020-11-10 09:06:55 -0700

    Rename spec, from exercises to days as it was more of a days test

--- spec/features/days/show_days_spec.rb
+++ spec/features/days/show_days_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.describe 'Index exercises', type: :feature do 
+RSpec.describe 'Days', type: :feature do 
   before :each do 
     @user_1 = User.create!(username: "user1", password: "test")
     @user_2 = User.create!(username: "user2", password: "test")
@@ -9,15 +9,22 @@ RSpec.describe 'Index exercises', type: :feature do
     @week_2 = Week.create!(name: "Week 2", workout: @workout_2)
   end
   
-  it "exercises display correctly" do 
+  it "Days display correctly" do 
+    day_1 = Day.create!(name: "Day 1", week: @week_1)
+    day_2 = Day.create!(name: "Day 2", week: @week_1)
+    day_3 = Day.create!(name: "Day 3", week: @week_1)
+    day_4 = Day.create!(name: "Day 4", week: @week_1)
+    day_5 = Day.create!(name: "Day 5", week: @week_1)
+    day_6 = Day.create!(name: "Day 6", week: @week_1)
+    day_7 = Day.create!(name: "Day 7", week: @week_1)
     visit '/'
     click_on "Login"
     fill_in :username, with: @user_1.username
     fill_in :password, with: @user_1.password
     click_on "Log In"
     visit "/weeks/#{@week_1.id}"
-    click_on "Create Days"
     click_on "Day 1"
+    expect(current_path).to eq("/days/#{day_1.id}")
     expect(page).to have_content("Day 1")
     click_on "Add Exercise"
   end 
