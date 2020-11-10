a01003264ee5ffd6a61c202a5a6ad54ff3500bef
Jack Gould <johnhilsmangould@gmail.com>
2020-11-09 17:16:16 -0700

    add active record associations to user and  workout

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true 
   validates_presence_of :password, require: true
 
+  has_many :workouts
+
   has_secure_password
 end 
