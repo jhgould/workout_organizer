cd57bb7b34b02590710858b54187e37661162a15
Jack Gould <johnhilsmangould@gmail.com>
2020-11-23 16:46:06 -0700

    add partial exercise delete spec

--- spec/features/exercises/delete_exercise_spec.rb
+++ spec/features/exercises/delete_exercise_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.describe 'Exercises', type: :feature do 
+RSpec.describe 'Exercises ', type: :feature do 
   before :each do 
     @user_1 = User.create!(username: "user1", password: "test")
     @user_2 = User.create!(username: "user2", password: "test")
@@ -22,28 +22,12 @@ RSpec.describe 'Exercises', type: :feature do
 
   end
   
-  
-  it "User can add exercise" do 
+  it "User can delete an exercise" do 
+    exercise_1 = Exercise.create!(category: "Upper Body", muscle_group: "Chest", name: "Bench Press", sets: 4, reps: 10, rest_time: 60, weight: 145, day: @day_1)
+    exercise_2 = Exercise.create!(category: "Upper Body", muscle_group: "Chest", name: "Incline Bench Press", sets: 4, reps: 10, rest_time: 60, weight: 145, day: @day_1)
+    exercise_3 = Exercise.create!(category: "Lower Body", muscle_group: "Legs", name: "Squats", sets: 4, reps: 10, rest_time: 60, weight: 145, day: @day_2)
     visit "/days/#{@day_1.id}"
-    click_on "Add Exercise"
-    # save_and_open_page
-    select('Upper Body', from: 'category')
-    select('Chest', from: 'muscle_group')
-    fill_in :name, with: "Bench Press"
-    fill_in :sets, with: 4
-    fill_in :reps, with: 10
-    fill_in :weight, with: 145
-    fill_in :rest_time, with: 60
-    
-    click_on "Add"
-    expect(current_path).to eq("/days/#{@day_1.id}")
-    #create within blocks to test better
-    expect(page).to have_content("Bench Press")
-    expect(page).to have_content("Weight: 145")
-    expect(page).to have_content("Sets: 4")
-    expect(page).to have_content("Reps: 10")
-    expect(page).to have_content("Rest: 60 seconds")
-    expect(page).to_not have_content("Squats")
+    #create within blocks so I can delete 
   end 
 
 end
