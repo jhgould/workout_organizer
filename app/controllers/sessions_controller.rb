d05a0ef4a05badc8bb10558195269418a407b553
Jack Gould <johnhilsmangould@gmail.com>
2020-11-30 15:18:54 -0700

    add spec for sad path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
 
   def create 
     @user = User.find_by(username: params[:username])
-    if @user.authenticate(params[:password])
+    if @user
+      @user.authenticate(params[:password])
       session[:user_id] = @user.id 
       # flash[:notice] = "Welcome, #{@user.username}!"
       redirect_to '/'
     else 
-      # flash[:error] = "Credintials are bad"
+      flash[:error] = "Credintials are bad"
       render :new
     end 
   end 
