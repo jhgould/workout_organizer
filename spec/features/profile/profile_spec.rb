9c49a8bcab3cbc61c20acceba7b44c45c4f07097
Jack Gould <johnhilsmangould@gmail.com>
2021-01-04 16:53:16 -0700

    update spec

--- spec/features/profile/profile_spec.rb
+++ spec/features/profile/profile_spec.rb
@@ -1,24 +1,11 @@
 require 'rails_helper'
 RSpec.describe 'User Profile', type: :feature do 
   before :each do 
-    @user_1 = User.create!(username: "user1", password: "test")
-    @user_2 = User.create!(username: "user2", password: "test")
-    @workout_1 = Workout.create!(name: "Workout 1", user: @user_1)
-    @workout_2 = Workout.create!(name: "Workout 1", user: @user_2)
-    @week_1 = Week.create!(name: "Week 1", workout: @workout_1)
-    @week_2 = Week.create!(name: "Week 2", workout: @workout_2)
-    @day_1 = Day.create!(name: "Day 1", week: @week_1)
-    @day_2 = Day.create!(name: "Day 2", week: @week_1)
-    @day_3 = Day.create!(name: "Day 3", week: @week_1)
-    @day_4 = Day.create!(name: "Day 4", week: @week_1)
-    @day_5 = Day.create!(name: "Day 5", week: @week_1)
-    @day_6 = Day.create!(name: "Day 6", week: @week_1)
-    @day_7 = Day.create!(name: "Day 7", week: @week_1)
     visit "/"
-    click_on "Login"
-    fill_in :username, with: @user_1.username
-    fill_in :password, with: @user_1.password
-    click_on "Log In"
+    click_on "Create User"
+    fill_in :username, with: "funbucket"
+    fill_in :password, with: "password"
+    click_on "Create"
   end
   
   it "Pofile link is located in nav bar and brings user to profile page" do 
