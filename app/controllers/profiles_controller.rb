22d66d77181335980406931448ea52fca6a3bbc7
Jack Gould <johnhilsmangould@gmail.com>
2021-01-11 10:31:52 -0700

    Add controller methods for update, edit, private

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,30 @@ class ProfilesController < ApplicationController
   def show 
     user = User.find(current_user.id)
     @profile = Profile.find_by(user_id: user.id)
+    if @profile.name == nil 
+      redirect_to "/profile/edit"
+    end 
   end 
 
+  def edit 
+  end 
+
+  def update 
+   user = User.find(current_user.id)
+   profile = Profile.find_by(user_id: user.id)
+   profile.update(profile_params)
+   redirect_to '/profile'
+  end 
+
+private 
+
+  def profile_params 
+    params.permit(
+      :name,
+      :age,
+      :height,
+      :weight,
+      :goal_weight
+    )
+  end 
 end 
