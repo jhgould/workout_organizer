0ab881be80454f68817e48e30870315921423011
Jack Gould <johnhilsmangould@gmail.com>
2021-01-11 10:05:57 -0700

    add update profile test

--- spec/features/profile/profile_spec.rb
+++ spec/features/profile/profile_spec.rb
@@ -11,9 +11,33 @@ RSpec.describe 'User Profile', type: :feature do
   it "Pofile link is located in nav bar and brings user to profile page" do 
     within '#top-nav' do 
       expect(page).to have_link("Profile")
+    end 
+  end 
+
+  it "User is redirected to form to update profile if it is not filled out" do 
+    within '#top-nav' do 
+      click_on "Profile"
+    end 
+    expect(current_path).to eq('/profile/edit')
+  end 
+
+  it "User can update profile from edit page" do 
+    within '#top-nav' do 
       click_on "Profile"
     end 
     
-    expect(current_path).to eq('/profile')
+    fill_in "name", with: "Fun Bucket"
+    fill_in "age", with: 26
+    fill_in "height", with: 6
+    fill_in "weight", with: 180
+    fill_in "goal_weight", with: 160
+    click_button "Submit"
+
+    expect(current_path).to eq("/profile")
+    expect(page).to have_content("Fun Bucket")
+    expect(page).to have_content(26)
+    expect(page).to have_content(6)
   end 
+
+
 end
