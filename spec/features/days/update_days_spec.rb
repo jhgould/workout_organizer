6df51a3075779294808d8e3b9739a9e751afff00
Jack Gould <johnhilsmangould@gmail.com>
2021-01-14 15:19:17 -0700

    add spec for updating day names

---
+++ spec/features/days/update_days_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+RSpec.describe 'Days', type: :feature do 
+  before :each do 
+    @user_1 = User.create!(username: "user1", password: "test")
+    @user_2 = User.create!(username: "user2", password: "test")
+    @workout_1 = Workout.create!(name: "Workout 1", user: @user_1)
+    @workout_2 = Workout.create!(name: "Workout 1", user: @user_2)
+    @week_1 = Week.create!(name: "Week 1", workout: @workout_1)
+    @week_2 = Week.create!(name: "Week 2", workout: @workout_2)
+    @profile1 = Profile.create!(name: "guy", age: 23, height: 4, weight: 5, user_id: @user_1.id)
+    @profile2 = Profile.create!(name: "guy1", age: 23, height: 4, weight: 5, user_id: @user_2.id)
+    @day_1 = Day.create!(name: "Day 1", week: @week_1)
+    @day_2 = Day.create!(name: "Day 2", week: @week_1)
+    @day_3 = Day.create!(name: "Day 3", week: @week_1)
+    @day_4 = Day.create!(name: "Day 4", week: @week_1)
+    @day_5 = Day.create!(name: "Day 5", week: @week_1)
+    @day_6 = Day.create!(name: "Day 6", week: @week_1)
+    @day_7 = Day.create!(name: "Day 7", week: @week_1)
+  end
+  
+  it "user can update day names" do 
+    visit "/"
+    click_on "Login"
+    fill_in :username, with: @user_1.username
+    fill_in :password, with: @user_1.password
+    click_on "Log In"
+
+    visit "/weeks/#{@week_1.id}"
+    
+    click_on "Edit"
+    fill_in :day_1, with: "Upper Body"
+    click_on "Submit"
+
+    expect(current_path).to eq("/weeks/#{@week_1.id}")
+    expect(page).to have_content("Upper Body")
+    expect(page).to have_content("Day 2")
+    expect(page).to have_content("Day 3")
+    expect(page).to have_content("Day 7")
+
+  end 
+end
