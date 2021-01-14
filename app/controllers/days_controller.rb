67ef8cca91fcecc69e1d25709831862fa5da9a16
Jack Gould <johnhilsmangould@gmail.com>
2021-01-14 16:16:08 -0700

    add update controller method to days controller

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -9,6 +9,28 @@ class DaysController < ApplicationController
     @day = Day.find(params[:id])
   end 
 
- 
+  def edit 
+    @week = Week.find(params[:week_id])
+  end 
+
+  def update 
+    week = Week.find(params[:week])
+    Day.update_day(week.days, update_params)
+    redirect_to "/weeks/#{week.id}"
+  end 
+
+ private 
+
+  def update_params 
+    params.permit(
+      :day_1,
+      :day_2,
+      :day_3,
+      :day_4,
+      :day_5,
+      :day_6,
+      :day_7
+    )
+  end 
 
 end 
