abb39a56359a876f7a41358de3449f133b9ccaa1
Jack Gould <johnhilsmangould@gmail.com>
2021-01-14 18:34:40 -0700

    rename method for better readablity

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -15,7 +15,7 @@ class DaysController < ApplicationController
 
   def update 
     week = Week.find(params[:week])
-    Day.update_day(week.days, update_params)
+    Day.update_day_names(week.days, update_params)
     redirect_to "/weeks/#{week.id}"
   end 
 
--- app/models/day.rb
+++ app/models/day.rb
@@ -10,12 +10,12 @@ class Day < ApplicationRecord
     end 
   end 
 
-  def self.update_day(days, update_params)
+  def self.update_day_names(days, update_params)
     counter = 1
     days.each do |day|
       update = "day_#{counter}"
-      day.name = update_params[update.to_sym]
-      day.save!
+       day.name = update_params[update.to_sym]
+        day.save!
       counter += 1
     end 
   end 
