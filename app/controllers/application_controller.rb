71dcfbaf9c84383feb9ec6ddf3328d73f68cc752
Jack Gould <johnhilsmangould@gmail.com>
2021-01-15 11:54:32 -0700

    add is_logged_in method to authenticate users before accessing random urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :error, :success, :notice
-  helper_method :current_user
+  helper_method :current_user, :is_logged_in
+   
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end 
 
+  def is_logged_in 
+    if current_user == nil 
+      render file: "/public/404"
+    end 
+  end 
 
 end
