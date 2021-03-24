537269f24253706e4006b2b94d01f7a9b3bd3dc3
Jack Gould <johnhilsmangould@gmail.com>
2021-03-23 20:42:07 -0600

    add home route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
 
+  get '/home', to: 'homes#index'
+
   get '/users/new', to: 'users#new'
   post '/users', to: "users#create"
 
